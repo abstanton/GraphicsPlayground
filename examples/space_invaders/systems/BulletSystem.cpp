9bfa77a80c5d970214ac4345944a5934dcae05b3
Angus Stanton <angusstanton225@gmail.com>
2022-11-07 21:14:12 +0000

    Space Invaders: Remove metal texts

--- examples/space_invaders/systems/BulletSystem.cpp
+++ examples/space_invaders/systems/BulletSystem.cpp
@@ -1,14 +1,18 @@
 #include "BulletSystem.h"
 
+#include <components/PointLight.h>
 #include <components/Transform.h>
 
 #include "../components/Bullet.h"
 #include "iostream"
 
 void BulletSystem::onUpdate(ecs::Registry& registry, float delta_time) {
-  registry.view<Bullet, Transform>().each([&](ecs::Entity ent, Bullet& bullet,
-                                              Transform& transform) {
+  registry.view<Bullet, Transform, PointLight>().each([&](ecs::Entity ent,
+                                                          Bullet& bullet,
+                                                          Transform& transform,
+                                                          PointLight& light) {
     transform.setPosition(transform.position() + bullet.velocity * delta_time);
+    light.position = transform.position();
     if (transform.position().y > 20.0f) {
       // TODO: Can I do this in a loop? not sure, i think I can cos it doesn't
       // move memory around afaik
